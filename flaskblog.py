f9103c93f4fe064743077adc82223ff189cd61d8
wloszynski <adrian@wloszynski.pl>
2020-08-08 13:27:16 +0200

    Full-Featured Web App Part 2 - Templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Adrian Wloszynski',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 7, 2020'
+    },
+    {
+    'author': 'Adrian Wloszynski',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'August 8, 2020'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
